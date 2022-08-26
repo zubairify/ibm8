2c4f520f412ed97f20042ee2f357d94fc19bb9b9
Zubair Shaikh <zubairify@gmail.com>
2022-08-26 12:57:40 +0530

    Updated Flight Boot

--- flight-boot/src/main/java/com/ibm/rest/FlightController.java
+++ flight-boot/src/main/java/com/ibm/rest/FlightController.java
@@ -22,32 +22,38 @@ public class FlightController {
 	@Autowired
 	private FlightService service;
 	
+	// http://localhost:8880/flight
 	@PostMapping(value = "/flight", consumes = "application/json")
 	public String addFlight(@RequestBody Flight f) {
 		int code = service.save(f);
 		return "Flight added with code: " + code;
 	}
-	
+
+	// http://localhost:8880/flight/123
 	@GetMapping(value = "/flight/{code}", produces = "application/json")
 	public Flight getFlight(@PathVariable int code) {
 		return service.fetch(code);
 	}
 	
+	// http://localhost:8880/flights
 	@GetMapping(value = "/flights", produces = "application/json")
 	public List<Flight> getAll() {
 		return service.list();
 	}
 	
+	// http://localhost:8880/carrier/Jet
 	@GetMapping(value = "/carrier/{carrier}", produces = "application/json")
 	public List<Flight> getByCarrier(@PathVariable String carrier) {
 		return service.byCarrier(carrier);
 	}
 	
+	// http://localhost:8880/route?source=Mumbai&destiny=Goa
 	@GetMapping(value = "/route", produces = "application/json")
 	public List<Flight> getByRoute(@RequestParam String source, @RequestParam String destiny) {
 		return service.byRoute(source, destiny);
 	}
 	
+	// http://localhost:8880/flight/123
 	@DeleteMapping(value = "/flight/{code}")
 	public String remove(@PathVariable int code) {
 		service.delete(code);
